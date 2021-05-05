40d6963b4002c6b3570bd822db2edb7fccedeb7a
Arni10 <arnycadabra@gmail.com>
2021-05-05 20:17:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firebasecloudmessaging/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.firebasecloudmessaging
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = "AAAAeo3paTc:APA91bGE2U7ZDZoqVy9jHJMmEX47Vu60BZVyHHlZ1oHax-ApNeiNvNacvbosItfhlTQADjKcNtUhfzsf_2SohxojxMKgKKbdDrDKEn4O0n1oucQCVcoNbUdeeH0qnHo6DcEq9kG4BR2A"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
